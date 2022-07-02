333aac3a6c4aa16c20b9ac6b5fdad07a3c85d36b
Ace <mohammed.ashfaq1127@gmail.com>
2022-07-02 15:59:19 +0530

    added Prime number using Function

---
+++ functions/src/com/ace/Primenumber_with_Functions.java
@@ -0,0 +1,29 @@
+package com.ace;
+
+import java.util.Scanner;
+
+public class Primenumber_with_Functions {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int num = in.nextInt();
+        String ans = prime(num);
+        System.out.println(ans);
+
+
+    }
+
+    static String prime(int num) {
+        int n = 2;
+        String isPrime = num + " is a prime number.";
+        String notPrime = num + " is NOT a prime number.";
+       double sqrt_of_num = Math.sqrt(num);
+        while(n <= sqrt_of_num){
+            if(num % n == 0){
+                return notPrime;
+            }
+            n++;
+        }
+        return isPrime;
+    }
+}
